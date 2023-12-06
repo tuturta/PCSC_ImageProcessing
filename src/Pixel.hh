2e7c8ddaadeebdf9b2946a7901152fc10520c0bf
Lulux4 <leagainon@gmail.com>
2023-12-06 12:01:44 +0100

    Pixel copy constructor and operators

--- src/Pixel.hh
+++ src/Pixel.hh
@@ -7,15 +7,20 @@ using namespace std;
 
 // Class Pixel : a pixel is a collection of N positive integers (e.g. N=1 for grayscale pixel, N=3 for RGB pixel,...)
 class Pixel{
-    // Constructor
+    // Constructors
     public :
         Pixel(initializer_list<unsigned int> const& val): values(val){}
         Pixel(vector<unsigned int> channels): values(channels){}
-    // Methods 
+        Pixel (Pixel const&P):values(P.values){} // constructeur de copie
+        
+    // Operators :
+        Pixel& operator=(const Pixel& P);
+        friend ostream & operator << (ostream &out, const Pixel &c);    // Methods 
     public : 
         size_t dim();
+        //void set_pixel_data(unsigned int new_data[3]);
+        unsigned int get_channel_value(size_t channel_id) const;
         void set_channel_value(size_t channel_id, unsigned int value);
-        unsigned int get_channel_value(size_t channel_id);
     // Attributes 
     private :
         vector<unsigned int> values;
