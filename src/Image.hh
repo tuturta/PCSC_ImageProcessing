c416253ce01c012aebf652a304691f145f41410c
Lulux4 <leagainon@gmail.com>
2023-12-15 05:34:16 +0100

    Doxygen  comments for classes Image and Compute

--- src/Image.hh
+++ src/Image.hh
@@ -7,7 +7,7 @@
 
 using Eigen::MatrixXcd;
 
-// Class image
+/// @brief 2D collection of Pixels, with one or more channels.
 class Image{
     // Constructors
     public: 
@@ -20,7 +20,7 @@ class Image{
         Image(Image const& img);
 
         /// @brief Construct an image from the matrices of its channels
-        /// @param bands_matrices vector<MatrixXcd>> where the MatrixScd's have all the same
+        /// @param bands_matrices vector<MatrixXcd>> where the MatrixXcd's have all the same
         Image(std::vector<MatrixXcd> const& bands_matrices,unsigned int max=255);
 
     // operators :
@@ -28,34 +28,75 @@ class Image{
 
     // Attributes
     private:
-        std::vector<std::vector<Pixel>> data;
-        size_t bands_number;
-        unsigned int max;
+        std::vector<std::vector<Pixel>> data; /// The image data is a 2D collection of Pixels
+        /// Number of channels
+        size_t bands_number;  
+        /// Maximal (possible) pixel channel value, e.g 255.
+        unsigned int max; 
     // Methods 
     public : 
+        /// @brief Provides the shape of the Image (number of columns and number of rows)
+        /// @return Returns a std::array<size_t,2> corresponding to (columns_number,rows_number)
         std::array<size_t,2> shape() const;
+        
+        /// @brief  Simple accessor for the number of channels 
+        /// @return Returns a size_t variable, corresponding to the bands_number attribute.
         size_t Nbands() const;
         
-        // basic getters 
+        /// @brief Simple accessor for the maximum pixel channel value 
+        /// @return unsigned int corresponding to the attribute 'max'.
         unsigned int get_max() const;
+        
+        /// @brief Simple accessor to the image data
+        /// @return std::vector<std::vector<Pixel>> corresponding to the attribute 'data'
         std::vector<std::vector<Pixel>> get_data() const;
         
-        // Pixelwise accessors
+        /// @brief Set a Pixel in the Image at a specified location
+        /// @param P Pixel to put in the Image
+        /// @param i row index
+        /// @param j column index 
         void set_pixel(Pixel const& P,unsigned int i, unsigned int j);
+        
+        /// @brief Access a Pixel in the Image at a specified location
+        /// @param i row index
+        /// @param j column index
+        /// @return Returns the wanted Pixel
         Pixel get_pixel(unsigned int i, unsigned int j) const;  
         
-        // Rows/columns accessors
+        /// @brief Set a row of Pixel in an Image based on its index
+        /// @param pix_row The row of Pixel to put in the Image
+        /// @param i The row index 
         void set_row(std::vector<Pixel> const& pix_row,unsigned int i);
+        
+        /// @brief Get a row of Pixel in an Image based on its index
+        /// @param i Row index 
+        /// @return The desired row of Pixel
         std::vector<Pixel> get_row(unsigned int i) const;
+        
+        /// @brief Set a column of Pixel in an Image based on its index
+        /// @param pix_col The column of Pixel to put in the Image
+        /// @param j The column index
         void set_column(std::vector<Pixel> const& pix_col,unsigned int j);
+        
+        /// @brief Get a column of Pixel in an Image based on its index
+        /// @param j  The column index
+        /// @return The desired column of Pixel
         std::vector<Pixel> get_column(unsigned int j) const;
 
-        // Get channels of the image as lists or matrices
+        /// @brief  Get channels of the image as a 1D collection of positive integers.
+        /// This method returns a band of the image as a list. The only argument is the band index.
+        /// @param b channel index
+        /// @return vector<unsigned int> All values of the image on channel 'b' from left to right, top to bottom, concatenaed in a 1D array.
         std::vector<unsigned int> get_band_1D(unsigned int b) const;
+        
+        /// @brief Provides a set of matrices, each of them containingone band of the image. 
+        /// @return std::vector<MatrixXcd> a vector of Eigen::MatrixXcd
         std::vector<MatrixXcd> bands_as_complex_matrices() const;
 
 };  
 
-// Function to convert a vector of MatrixXcd to a vector<vector<Pixel>>
+/// @brief Function that converts a vector of MatrixXcd to a vector<vector<Pixel>>
+/// @param bands_mat The matrices for each channel
+/// @return std::vector<std::vector<Pixel>>, which can be used to construct an Image.
 std::vector<std::vector<Pixel>> bands_matrix_to_2D_pixel_vector(std::vector<MatrixXcd> const& bands_mat);
 
--- src/compute.hh
+++ src/compute.hh
@@ -1,17 +1,33 @@
 #pragma once
 #include "Image.hh"
 
+/// Parent class for classes used for extracting informations from Images
+/**
+ * Has no methods and only one attribute, a const reference to an Image. The purpose of this class is to be inherited by daughter classes as 
+*/
 class Compute{
     public:
+    /// Constructor of the class Compute
     Compute(Image const& img);
 
     protected:
     Image const& data;
 };
 
+/// Class used to compute intensity histograms of Images
+/**
+ * Inherits from class Compute. 
+ * This class owns a method compute, which computes the histogram of the Image pointed by the referenced data. 
+*/
 class ComputeHistogram : public Compute {
     public:
+    /// @brief Constructor of class ComputeHistogram
+    /// @param img Const reference to the Image on which to compute the histogram
+    /// @param Nbins Positive integer - This parameter determines the number of bins in the histogram to compute
     ComputeHistogram(Image const& img,unsigned int Nbins);
+    
+    ///@brief Computes the intensity histograms for each channel of an image.
+    ///@return std::vector<std::vector<double>> vector containing the histograms for each channel
     std::vector<std::vector<double>> compute() const;
     
     private:
