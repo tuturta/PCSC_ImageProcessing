41526bfb517e38aa2be4f3a33d801abec4a1a662
tuturta <61622056+tuturta@users.noreply.github.com>
2023-12-15 04:46:31 +0100

    Doxygen documentation

--- src/reader.hh
+++ src/reader.hh
@@ -1,27 +1,64 @@
 #pragma once
+
 #include "Image.hh"
 #include <vector>
 #include <string>
 
 using namespace std;
 
-
+/**
+ * @brief Abstract base class for image readers.
+ */
 class AbstractReader {
-    public:
-        AbstractReader(string_view path);
-        virtual Image read() = 0;
-    protected:
-        const string path_;
+public:
+    /**
+     * @brief Constructor for AbstractReader.
+     * @param path The path to the file to be read.
+     */
+    explicit AbstractReader(string_view path);
+
+    /**
+     * @brief Pure virtual function to read an Image.
+     * @return The Image read from the file.
+     */
+    virtual Image read() = 0;
+
+protected:
+    const string path_; /**< Path to the file. */
 };
 
-class TXTReader : public AbstractReader{
-    public:
-        TXTReader(string_view path);
-        Image read() override;
+/**
+ * @brief Implementation of a text file reader for images.
+ */
+class TXTReader : public AbstractReader {
+public:
+    /**
+     * @brief Constructor for TXTReader.
+     * @param path The path to the text file to be read.
+     */
+    explicit TXTReader(string_view path);
+
+    /**
+     * @brief Read the Image data from a text file.
+     * @return The Image read from the file.
+     */
+    Image read() override;
 };
 
-class PPMReader : public AbstractReader{
-    public:
-        PPMReader(string_view path);
-        Image read() override;
+/**
+ * @brief Implementation of a PPM file reader for images.
+ */
+class PPMReader : public AbstractReader {
+public:
+    /**
+     * @brief Constructor for PPMReader.
+     * @param path The path to the PPM file to be read.
+     */
+    explicit PPMReader(string_view path);
+
+    /**
+     * @brief Read the Image data from a PPM file.
+     * @return The Image read from the file.
+     */
+    Image read() override;
 };
--- src/writer.hh
+++ src/writer.hh
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "Image.hh"
 #include <vector>
 #include <fstream>
@@ -5,23 +7,59 @@
 
 using namespace std;
 
-
+/**
+ * @brief Abstract base class for image writers.
+ */
 class AbstractWriter {
-    public:
-        AbstractWriter(string_view path);
-        virtual void write(const Image& image) = 0;
-    protected:
-        const string path_;
+public:
+    /**
+     * @brief Constructor for AbstractWriter.
+     * @param path The path to the file to be written.
+     */
+    explicit AbstractWriter(string_view path);
+
+    /**
+     * @brief Pure virtual function to write an Image.
+     * @param image The Image to be written.
+     */
+    virtual void write(const Image& image) = 0;
+
+protected:
+    const string path_; /**< Path to the file. */
 };
 
-class TXTWriter : public AbstractWriter{
-    public:
-        TXTWriter(string_view path);
-        void write(const Image& image) override;
+/**
+ * @brief Implementation of a text file writer for images.
+ */
+class TXTWriter : public AbstractWriter {
+public:
+    /**
+     * @brief Constructor for TXTWriter.
+     * @param path The path to the text file to be written.
+     */
+    explicit TXTWriter(string_view path);
+
+    /**
+     * @brief Write the Image data to a text file.
+     * @param image The Image to be written.
+     */
+    void write(const Image& image) override;
 };
 
-class PPMWriter : public AbstractWriter{
-    public:
-        PPMWriter(string_view path);
-        void write(const Image& image) override;
+/**
+ * @brief Implementation of a PPM file writer for images.
+ */
+class PPMWriter : public AbstractWriter {
+public:
+    /**
+     * @brief Constructor for PPMWriter.
+     * @param path The path to the PPM file to be written.
+     */
+    explicit PPMWriter(string_view path);
+
+    /**
+     * @brief Write the Image data to a PPM file.
+     * @param image The Image to be written.
+     */
+    void write(const Image& image) override;
 };
