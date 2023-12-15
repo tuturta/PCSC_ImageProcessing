0a77c578192838434de5849a0b49f4e2651170dd
tuturta <61622056+tuturta@users.noreply.github.com>
2023-12-15 04:47:24 +0100

    Implementation of Gaussian Filter and Contour Extraction classes

--- src/transform.cpp
+++ src/transform.cpp
@@ -5,8 +5,8 @@
 #include <iostream>
 
 using namespace std;
-using namespace Eigen;
-
+using Eigen::MatrixXcd;
+using Eigen::VectorXcd;
 
 bool padToPowerOfTwo(Eigen::VectorXcd& input) {
     size_t new_size = 1;
@@ -43,20 +43,33 @@ bool padToPowerOfTwo(Eigen::MatrixXcd& input) {
 }
 
 
+/*========Classes FourierTransform=======*/
 
-void FourierTransform::transform(MatrixXcd& image){
-
+void FourierTransform::fwd(MatrixXcd& image){
     // Shape of the image
     const size_t nb_rows(image.rows());
     const size_t nb_cols(image.cols());
+
+    // test if the size is a power of two
+    size_t test_power_of_two(1);
+    while(test_power_of_two<nb_rows){
+        test_power_of_two*=2;
+    }
+    if(test_power_of_two != nb_rows) throw std::domain_error("The number of rows is " + to_string(nb_rows) + " which is not a power of two !");
     
+    test_power_of_two = 1;
+    while(test_power_of_two<nb_cols){
+        test_power_of_two*=2;
+    }
+    if(test_power_of_two != nb_cols) throw std::domain_error("The number of cols is " + to_string(nb_rows) + " which is not a power of two !");
+
     // Declare an empty row 
     VectorXcd row;
 
     // Apply FFT on every rows
     for(size_t i(0); i<nb_rows; ++i){
         row = image.row(i);
-        this->transform(row);
+        this->fwd(row);
         image.row(i) = row;
     }   
 
@@ -66,7 +79,7 @@ void FourierTransform::transform(MatrixXcd& image){
     // Apply FFT on every cols (columns are now rows because of the transposition)
     for(size_t i(0); i<nb_cols; ++i){
         row = image.row(i);
-        this->transform(row);
+        this->fwd(row);
         image.row(i) = row;
     }  
 
@@ -74,7 +87,7 @@ void FourierTransform::transform(MatrixXcd& image){
     image.transposeInPlace();
 }
 
-void FourierTransform::inverse_transform(MatrixXcd& image){
+void FourierTransform::inv(MatrixXcd& image){
 
 
     // Shape of the image
@@ -90,7 +103,7 @@ void FourierTransform::inverse_transform(MatrixXcd& image){
     // Apply FFT on every rows
     for(size_t i(0); i<nb_cols; ++i){
         row = image.row(i);
-        this->inverse_transform(row);
+        this->inv(row);
         image.row(i) = row;
     }   
 
@@ -100,7 +113,7 @@ void FourierTransform::inverse_transform(MatrixXcd& image){
     // Apply FFT on every cols (columns are now rows because of the transposition)
     for(size_t i(0); i<nb_rows; ++i){
         row = image.row(i);
-        this->inverse_transform(row);
+        this->inv(row);
         image.row(i) = row;
     }  
 
@@ -109,13 +122,13 @@ void FourierTransform::inverse_transform(MatrixXcd& image){
 
 
 // inverse Fourier Transform
-void FourierTransform::inverse_transform(VectorXcd& complex_vector)
+void FourierTransform::inv(VectorXcd& complex_vector)
 {   
     // conjugate the complex numbers
     complex_vector.conjugate();
 
     // forward fft
-    this->transform(complex_vector);
+    this->fwd(complex_vector);
  
     // conjugate the complex numbers and scale it
     complex_vector.conjugate();
@@ -128,7 +141,7 @@ void FourierTransform::inverse_transform(VectorXcd& complex_vector)
     complex_vector.reverseInPlace();
 }
 
-void FFT::transform(VectorXcd& input){
+void FFT::fwd(VectorXcd& input){
     const size_t len(input.size());
 
     // Check if it is splitted enough
@@ -146,8 +159,8 @@ void FFT::transform(VectorXcd& input){
     }
 
     // Recursively call the Fourier Transform until the len is 1 
-    this->transform(even);
-    this->transform(odd);
+    this->fwd(even);
+    this->fwd(odd);
 
     // Calculate DFT
 	for (size_t k = 0; k < len / 2; k++) {
@@ -160,7 +173,7 @@ void FFT::transform(VectorXcd& input){
 
 
 /*DFT : Exact fourier transform*/
-void DFT::transform(VectorXcd& input)
+void DFT::fwd(VectorXcd& input)
 {
 	complex<double> temp;
     const complex<double> i(0.,1.);
@@ -178,10 +191,124 @@ void DFT::transform(VectorXcd& input)
 	}
 }
 
-void FFT::transform(MatrixXcd& image){
-    FourierTransform::transform(image);
+void FFT::fwd(MatrixXcd& image){
+    FourierTransform::fwd(image);
 }
 
-void DFT::transform(MatrixXcd& image){
-    FourierTransform::transform(image);
+void DFT::fwd(MatrixXcd& image){
+    FourierTransform::fwd(image);
+}
+
+/*=======Classes Filter=======*/
+
+Filter::Filter(Image& image): image_(image) {}
+ContourExtraction::ContourExtraction(Image& image): Filter(image) {
+    array<size_t, 2UL> shape = image_.shape();
+    if((shape[0]>3) or shape[1]>3) throw invalid_argument("Image can not be inferior to 3x3 !");
+}
+GaussianFilter::GaussianFilter(Image& image): Filter(image) {}
+GaussianFilter::GaussianFilter(Image& image, double sigma, size_t filter_size): Filter(image), sigma_(sigma), filter_size_(filter_size){
+    array<size_t, 2UL> shape = image_.shape();
+    if((filter_size > shape[0]) or (filter_size > shape[1])) throw invalid_argument("The filter_size is larger than the image !");
+    if(sigma < 0) throw invalid_argument("Sigma can not be negative !");
+}
+
+
+void ContourExtraction::apply(){
+
+    // Extract the different channels
+    vector<MatrixXcd> images = image_.bands_as_complex_matrices();
+
+    // Compute size of image 
+    size_t nb_cols = images[0].cols();
+    size_t nb_rows = images[0].rows();
+
+    // Sobel operator for horizontal and vertical edges
+    MatrixXcd sobelX(3, 3);
+    sobelX << -1, 0, 1,
+              -2, 0, 2,
+              -1, 0, 1;
+
+    MatrixXcd sobelY = sobelX.transpose();
+
+    // Apply sobel filter to all images
+    MatrixXcd edgesX;
+    MatrixXcd edgesY;
+    for(auto& image : images){
+        edgesX = applyFilter(image, sobelX);
+        edgesY = applyFilter(image, sobelY);
+        image = (edgesX.array().abs() + edgesY.array().abs()).matrix();
+    }
+
+    // Modify the attribute image
+    image_ = Image(images); 
+}
+
+void GaussianFilter::apply(){
+   
+    // Extract the different channels
+    vector<MatrixXcd> images = image_.bands_as_complex_matrices();
+
+    // Parameters of the filter
+    size_t filterSize = 3;  // Adjust the filter size as needed
+    double sigma(1.0);      // Adjust the sigma value as needed
+    
+    // Create a 2D Gaussian filter
+    MatrixXcd gaussianFilter(filterSize, filterSize);
+    
+    for (int i = 0; i < filterSize; ++i) {
+        for (int j = 0; j < filterSize; ++j) {
+            int x = i - filterSize / 2;
+            int y = j - filterSize / 2;
+            gaussianFilter(i, j) = std::exp(-(x * x + y * y) / (2 * sigma * sigma));
+        }
+    }
+
+    // Normalize the Gaussian filter
+    gaussianFilter /= gaussianFilter.sum();
+
+    // Filtered images
+    for(auto& image : images){
+        image = applyFilter(image, gaussianFilter);
+    }
+
+    // Modify the attribute image
+    image_ = Image(images);
+}
+
+MatrixXcd applyFilter(const MatrixXcd& input, const MatrixXcd& filter) {
+
+    if (filter.size() > input.size()) throw std::domain_error("The filter is larger than the image !");
+
+    // Apply 2D FFT to the input matrix
+    FFT fft;
+
+    // Zero padding to apply filter
+    size_t padding_size = filter.rows()/2;
+    MatrixXcd padded_matrix(input);
+
+
+    // Padding in order to have size of power of 2 to Cooley Tukey Algoritm
+    bool padding = padToPowerOfTwo(padded_matrix);     // Add padding to use FFT 
+    fft.fwd(padded_matrix);
+
+    // Pad the filter
+    MatrixXcd filterX = MatrixXcd::Zero(padded_matrix.rows(), padded_matrix.cols());
+    filterX.topLeftCorner(filter.rows(), filter.cols()) = filter;
+
+    // Create frequency domain representations of operators
+    fft.fwd(filterX);
+
+    // Multiply in frequency domain (equivalent to convolution in spatial domain)
+    padded_matrix = padded_matrix.cwiseProduct(filterX);
+
+    // Apply inverse FFT to get back to spatial domain
+    fft.inv(padded_matrix);
+
+    // Remove the padding
+    MatrixXcd out_matrix(input);
+    out_matrix.block(padding_size+1, padding_size+1, out_matrix.rows()-padding_size-1, out_matrix.cols() - padding_size -1) =
+    padded_matrix.block(padding_size+1, padding_size+1, out_matrix.rows()-padding_size-1, out_matrix.cols() - padding_size -1);
+
+    return out_matrix;
 }
