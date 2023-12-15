d063ce9b04b5d982dbe6f36b8671e22f6f44b231
tuturta <61622056+tuturta@users.noreply.github.com>
2023-12-15 13:12:46 +0100

    Add new exceptions

--- src/MyExceptions.hh
+++ src/MyExceptions.hh
@@ -12,3 +12,28 @@ class InvalidDimException : public std::runtime_error {
 public:
     explicit InvalidDimException(std::string const& msg) : std::runtime_error(msg) {}
 };
+
+class NegativeDimException : public std::invalid_argument {
+public:
+    explicit NegativeDimException(std::string const& msg) : std::invalid_argument(msg) {}
+};
+
+class NegativePixelValueException : public std::invalid_argument {
+public:
+    explicit NegativePixelValueException(std::string const& msg) : std::invalid_argument(msg) {}
+};
+
+class WrongPpmFormatException : public std::invalid_argument {
+public:
+    explicit WrongPpmFormatException(std::string const& msg) : std::invalid_argument(msg) {}
+};
+
+class WrongFileFormatException : public std::invalid_argument {
+public:
+    explicit WrongFileFormatException(std::string const& msg) : std::invalid_argument(msg) {}
+};
+
+class PixelDimException : public std::domain_error {
+public:
+    explicit PixelDimException(std::string const& msg) : std::domain_error(msg) {}
+};
